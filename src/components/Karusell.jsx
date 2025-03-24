4cce33182e9605151255e48360748998e0996089
Aage Barlund <barlund@gmail.com>
2025-03-24 23:00:48 +0100

    20250324-2

---
+++ src/components/Karusell.jsx
@@ -0,0 +1,30 @@
+import { CCarousel, CCarouselCaption, CCarouselItem, CImage } from '@coreui/react'
+import '@coreui/coreui/dist/css/coreui.min.css'
+
+export const Karusell = () => {
+  return (
+    <CCarousel controls indicators className='m-3 border-4 border-y-slate-400 border-x-slate-500'>
+      <CCarouselItem>
+        <CImage className="d-block w-100" src="/src/images/Franklin1929I.jpg" alt="slide 1" />
+        <CCarouselCaption className="d-none d-md-block backdrop-blur-md border-1 border-slate-400 rounded-lg">
+          <h5 className='font-bold'>1929 Franklin</h5>
+          <p>Eier: Sverre Haugesten</p>
+        </CCarouselCaption>
+      </CCarouselItem>
+      <CCarouselItem>
+        <CImage className="d-block w-100" src="/src/images/Franklin1929II.jpg" alt="slide 2" />
+        <CCarouselCaption className="d-none d-md-block backdrop-blur-md border-1 border-slate-400 rounded-lg">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </CCarouselCaption>
+      </CCarouselItem>
+      <CCarouselItem>
+        <CImage className="d-block w-100" src="/src/images/FranklinRondane2.jpeg" alt="slide 3" />
+        <CCarouselCaption className="d-none d-md-block backdrop-blur-md border-1 border-slate-400 rounded-lg">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </CCarouselCaption>
+      </CCarouselItem>
+    </CCarousel>
+  )
+}
