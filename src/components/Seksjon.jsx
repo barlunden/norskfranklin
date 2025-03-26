f62d94eb31163756d9acedb6a573afc3a12c009e
Aage Barlund <barlund@gmail.com>
2025-03-26 20:26:28 +0100

    xs

--- src/components/Seksjon.jsx
+++ src/components/Seksjon.jsx
@@ -3,7 +3,7 @@ import { Panel } from "rsuite";
 export default function Seksjon() {
   return (
     <div class="">
-      <Panel header="Aktiviteter" shaded className="bg-gradient-to-br from-slate-100 to-slate-400">
+      <Panel header="Aktiviteter" shaded className="bg-slate-200">
           <div>
             <p>
             Her kommer det en oversikt over løp og andre aktiviteter en eller flere Frankliner deltar på.
