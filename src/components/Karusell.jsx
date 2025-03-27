6d672d1c3b6b46713ff9398f6ec0c0a3c8ed9917
Aage Barlund <barlund@gmail.com>
2025-03-27 22:57:59 +0100

    20250327-3
    Justeringar av layout.

--- src/components/Karusell.jsx
+++ src/components/Karusell.jsx
@@ -4,26 +4,26 @@ import '@coreui/coreui/dist/css/coreui.min.css'
 
 export const Karusell = () => {
   return (
-    <CCarousel controls indicators className='mx-24 border-1'>
+    <CCarousel controls indicators className='mx-24 border-1 hidden md:block'>
       <CCarouselItem>
         <CImage className="d-block w-100" src="/src/images/Franklin1929I.jpg" alt="1929 Franklin" />
         <CCarouselCaption className="backdrop-blur-md border-1 border-slate-400 rounded-lg">
           <h5 className='font-bold'>1929 Franklin</h5>
-          <p>Eier: Sverre Haugesten</p>
+          <p className='hidden lg:inline'>Eier: Sverre Haugesten</p>
         </CCarouselCaption>
       </CCarouselItem>
       <CCarouselItem>
         <CImage className="d-block w-100" src="/src/images/Franklin1929II.jpg" alt="1929 Franklin" />
         <CCarouselCaption className="backdrop-blur-md border-1 border-slate-400 rounded-lg">
           <h5>1929 Franklin</h5>
-          <p>Eier: Sverre Haugesten</p>
+          <p className='hidden lg:inline'>Eier: Sverre Haugesten</p>
         </CCarouselCaption>
       </CCarouselItem>
       <CCarouselItem>
         <CImage className="d-block w-100" src="/src/images/FranklinRondane2.jpeg" alt="slide 3" />
         <CCarouselCaption className="backdrop-blur-md border-1 border-slate-400 rounded-lg">
           <h5>Franklin 9B Touring</h5>
-          <p>Eier: Trude Bjerkeseth</p>
+          <p className='hidden lg:inline'>Eier: Trude Bjerkeseth</p>
         </CCarouselCaption>
       </CCarouselItem>
     </CCarousel>
