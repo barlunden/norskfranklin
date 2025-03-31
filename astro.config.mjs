a556acbe4f299222613b448187759a9bc89e7ad0
Aage Barlund <barlund@gmail.com>
2025-03-31 10:59:17 +0200

    edit: config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: netlify({}),
   site: "https://norskfranklinregister.netlify.app",
   
   integrations: [react()],
