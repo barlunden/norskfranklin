71df323943ea5731d6af320274350d1ab780e35f
Aage Barlund <barlund@gmail.com>
2025-03-31 10:46:49 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  site: "https://norskfranklinregister.netlify.app",
+  
   integrations: [react()],
 
   vite: {
