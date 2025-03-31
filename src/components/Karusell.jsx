3c2e4c445ae1583139412656133187c3eb81e3d2
Aage Barlund <barlund@gmail.com>
2025-03-31 11:03:44 +0200

    Update Karusell.jsx

--- src/components/Karusell.jsx
+++ src/components/Karusell.jsx
@@ -6,21 +6,21 @@ export const Karusell = () => {
   return (
     <CCarousel controls indicators className='mx-24 border-1 hidden md:block'>
       <CCarouselItem>
-        <CImage className="d-block w-100" src="/src/images/Franklin1929I.jpg" alt="1929 Franklin" />
+        <CImage className="d-block w-100" src="/images/Franklin1929I.jpg" alt="1929 Franklin" />
         <CCarouselCaption className="backdrop-blur-md border-1 border-slate-400 rounded-lg">
           <h5 className='font-bold'>1929 Franklin</h5>
           <p className='hidden lg:inline'>Eier: Sverre Haugesten</p>
         </CCarouselCaption>
       </CCarouselItem>
       <CCarouselItem>
-        <CImage className="d-block w-100" src="/src/images/Franklin1929II.jpg" alt="1929 Franklin" />
+        <CImage className="d-block w-100" src="/images/Franklin1929II.jpg" alt="1929 Franklin" />
         <CCarouselCaption className="backdrop-blur-md border-1 border-slate-400 rounded-lg">
           <h5>1929 Franklin</h5>
           <p className='hidden lg:inline'>Eier: Sverre Haugesten</p>
         </CCarouselCaption>
       </CCarouselItem>
       <CCarouselItem>
-        <CImage className="d-block w-100" src="/src/images/FranklinRondane2.jpeg" alt="slide 3" />
+        <CImage className="d-block w-100" src="/images/FranklinRondane2.jpeg" alt="slide 3" />
         <CCarouselCaption className="backdrop-blur-md border-1 border-slate-400 rounded-lg">
           <h5>Franklin 9B Touring</h5>
           <p className='hidden lg:inline'>Eier: Trude Bjerkeseth</p>
