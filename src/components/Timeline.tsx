35b3b82bfac0c9f7db2a9341f57d2eb01363d906
Aage Barlund <barlund@gmail.com>
2025-03-31 15:46:35 +0200

    lita oppdatering

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,7 +2,7 @@ import { Timeline } from "rsuite";
 import 'rsuite/dist/rsuite-no-reset.min.css';
 import { timelineData } from "../assets/TimelineData";
 
-export default function TimelineTest() {
+export default function FrankliTimeline() {
   return (
     <div className="w-1/2 mx-auto">
       <Timeline align="alternate">
