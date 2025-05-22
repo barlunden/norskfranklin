ae7249345668886c8933ba8c5bfffe2b7bd0389c
Aage Barlund <barlund@gmail.com>
2025-05-22 13:04:37 +0200

    Restart

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
